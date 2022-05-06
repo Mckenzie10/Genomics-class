332d73f842c7f664bbdd76f5e8f71d2be0172bfa
McKenzie10 <mckenzie.moles10@okstate.edu>
2022-05-06 00:45:47 +0000

    We finally getting somewhere

--- Thymoma cancer.R
+++ Thymoma cancer.R
@@ -15,17 +15,17 @@ all.Received = F
 page.Counter = 1
 page.size = 150
 THYM.Pats = list()
-while(all.Received == F){
-  THYM.Pats[[page.Counter]] = Samples.Clinical(format = "csv",cohort = cancer.Type, page_size = page.size,page = page.Counter)                                               
+while(all.Received == F)
+THYM.Pats[[page.Counter]] = Samples.Clinical(format = "csv",cohort = cancer.Type, page_size = page.size,page = page.Counter)                                               
   if(page.Counter > 1)  
-    colnames(THYM.Pats[[page.Counter]]) = colnames(THYM.Pats[[page.Counter-1]]    
-                                                   if(nrow(THYM.Pats[[page.Counter]])<page.size){all.Received = T} else{page.Counter=page.Counter+1}   
-                                                   THYM.Pats = do.call(rbind, THYM.Pats)
-                                                   dim(THYM.Pats)
-                                                   THYM.Pats = THYM.Pats[ which(THYM.Pats$vital_status == "dead"), ]
-                                                   diff.Exp.Genes = c("ESR1", "GATA3", "XBP1", "FOXA1", "ERBB2", "GRB7", "EGFR","FOXC1", "MYC")                                                     
-                                                   all.Found = F
-                                                   page.Counter = 1
-                                                   mRNA.Exp = list()
-                                                   page.Size = 2000
-                                                   while(all.Found == F){mRNA.Exp[[page.Counter]] = Samples.mRNASeq(format = "csv",mRNA.Exp[[page.Counter]] = Samples.mRNASeq(format = "csv", mRNA.Exp[[page.Counter]] = Samples.mRNASeq(format = "csv",gene = diff.Exp.Genes,cohort = "THYM",tcga_participant_barcode =tcga_participant_barcode =THYM.Pats$tcga_participant_barcode,page_size = page.Size,page_size = page.Size,page = page.Counter)
+colnames(THYM.Pats[[page.Counter]]) = colnames(THYM.Pats[[page.Counter-1]])    
+if(nrow(THYM.Pats[[page.Counter]])<page.size){all.Received = T} else{page.Counter=page.Counter+1}   
+THYM.Pats = do.call(rbind, THYM.Pats)
+dim(THYM.Pats)
+THYM.Pats = THYM.Pats[ which(THYM.Pats$vital_status == "dead"), ]
+diff.Exp.Genes = c("ESR1", "GATA3", "XBP1", "FOXA1", "ERBB2", "GRB7", "EGFR","FOXC1", "MYC")                                                     
+all.Found = F 
+page.Counter= 1
+mRNA.Exp = list()
+page.Size = 2000
+while(all.Found == F){mRNA.Exp[[page.Counter]] = Samples.mRNASeq}(format = "csv",mRNA.Exp[[page.Counter]] = Samples.mRNASeq)(format = "csv", mRNA.Exp[[page.Counter]]) = Samples.mRNASeq(format = "csv",gene = diff.Exp.Genes,cohort = "THYM",tcga_participant_barcode =tcga_participant_barcode =THYM.Pats$tcga_participant_barcode,page_size = page.Size,page_size = page.Size,page = page.Counter)
