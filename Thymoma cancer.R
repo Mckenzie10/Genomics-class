6c14fef20718fd20e70aa7f2969080cc035d611d
McKenzie10 <mckenzie.moles10@okstate.edu>
2022-05-09 01:50:24 +0000

    FINISHED PARTY! also nothing shows up in plot but everything runs great

--- Thymoma cancer.R
+++ Thymoma cancer.R
@@ -16,25 +16,32 @@ page.Counter = 1
 page.size = 150
 THYM.Pats = list(10)
 while(all.Received == F)
-THYM.Pats[[page.Counter]] = Samples.Clinical(format = "csv",cohort = cancer.Type, page_size = page.size,page = page.Counter)                                               
-  if(page.Counter > 1)  
-colnames(THYM.Pats[[page.Counter]])   
+  THYM.Pats[[page.Counter]] = Samples.Clinical(format = "csv",cohort = cancer.Type, page_size = page.size,page = page.Counter)                                               
+if(page.Counter > 1)  
+  colnames(THYM.Pats[[page.Counter]])   
 if(nrow(THYM.Pats[[page.Counter]])<page.size){all.Received = T} else{page.Counter=page.Counter+1}   
 THYM.Pats = do.call(rbind, THYM.Pats)
 dim(THYM.Pats)
 THYM.Pats = THYM.Pats[ which(THYM.Pats$vital_status == "dead"), ]
-diff.Exp.Genes = c("FOXD1", "NRAS", "DHX33", "ATRN", "HRAS", "GTF21", "TP53","CAPNS1")                                                     
-all.Found = F 
+diff.Exp.Genes = c("FOXD", "NRAS", "DHX33", "ATRN", "HRAS", "GTF21", "TP53","CAPNS1")                                                     
+all.Found = F  
 page.Counter= 1
 mRNA.Exp = list()
 page.Size = 2000
-while(all.Found=F){mRNA.Exp[[page.Counter]]=Samples.mRNASeq}(format="csv") gene(diff.Exp.Genes, cohort ="THYM") tcga_participant_barcode=THYM.Pats$tcga_participant_barcode=Counter
+while(all.Found==F) {mRNA.Exp[[page.Counter]]= Samples.mRNASeq(format="csv", gene=diff.Exp.Genes, cohort="THYM", tcga_participant_barcode = THYM.Pats$tcga_participant_barcode, page_size = page.Size, page=page.Counter)}
+if(nrow(mRNA.Exp[[page.Counter]])<page.Size)
+  all.found=T
+else
+  page.Counter=page.Counter+1  
+mRNA.Exp=do.call(rbind, mRNA.Exp)
+dim(mRNA.Exp)
+r.Metadata.SampleTypes("csv")
+normal.Tissue.Pats=which(mRNA.Exp$sample_type=="NT")
+patient.Barcodes=mRNA.Exp$tcga_participant_barcode[normal.Tissue.Pats]
+mRNA.Exp=mRNA.Exp[which(mRNA.Exp$tcga_participant_barcode %in% patient.Barcodes & mRNA.Exp$sample_type %in% c("NT","TP")), ]
 library(ggplot2)
-data<-THYM.Pats
-ggplot2::aes_auto()+boxplot(data="THYM.Pats")
-library(haven)
-library(ggplot2)  
-library(directlabels)
-library(plotly)
-ggplot(data=NULL*aes=mapping(x=mediumorchid1)) environment(fun=parent.frame)
+p=ggplot(mRNA.Exp, aes(factor(gene), z.score))
+p+geom_boxplot(aes(fill = factor(sample_type))) +
+  scale_y_continuous(limits= c(-1, 5)) 
+scale_fill_discrete(name = "Tissue")  
 
